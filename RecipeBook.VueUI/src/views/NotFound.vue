b2c666d8ee93b1548da1ca9c9616d5bb2c75a1b6
chris <chris@email.com>
2021-12-14 00:39:59 -0600

    forgot a file

--- RecipeBook.VueUI/src/views/NotFound.vue
+++ RecipeBook.VueUI/src/views/NotFound.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container text-start">
     <div class="p-4 my-3 bg-light rounded-3">
-			<h2>404</h2>
-			<p>Requested page was not found</p>
-		</div>
+		<h2>404</h2>
+		<p>Requested page was not found</p>
+	</div>
   </div>
 </template>
 
