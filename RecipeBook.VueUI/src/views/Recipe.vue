19ac2a21b9155ebe7bb42db8804b3ab16d1d46a7
chris <chris.lu06@gmail.com>
2021-12-14 00:46:55 -0600

    formatting changes

--- RecipeBook.VueUI/src/views/Recipe.vue
+++ RecipeBook.VueUI/src/views/Recipe.vue
@@ -3,20 +3,25 @@
 		<div class="row border p-4 my-3 bg-light rounded-3">
 			<h3>{{ currentRecipe.name }}</h3>
 
-
 			<b-card class="w-100">
 				<b-tabs v-model="tabIndex" class="w-100">
 					<b-tab title="Text View" href="text-view">
-						<recipe-text-view :recipe-name="currentRecipe.name"
-										:description="currentRecipe.description"
-										:cook-time-minutes="currentRecipe.cookTimeMinutes"
-										:notes="currentRecipe.notes"
-										:ingredients-text="ingredientsText"
-										:directions-text="currentRecipe.directions"></recipe-text-view>
+						<recipe-text-view
+							:recipe-name="currentRecipe.name"
+							:description="currentRecipe.description"
+							:cook-time-minutes="currentRecipe.cookTimeMinutes"
+							:notes="currentRecipe.notes"
+							:ingredients-text="ingredientsText"
+							:directions-text="
+								currentRecipe.directions
+							"></recipe-text-view>
 					</b-tab>
 
 					<b-tab title="Edit View" href="edit-view">
-							<recipe-edit-view :recipe-id="currentRecipe.recipeId"></recipe-edit-view>
+						<recipe-edit-view
+							:recipe-id="
+								currentRecipe.recipeId
+							"></recipe-edit-view>
 					</b-tab>
 				</b-tabs>
 			</b-card>
@@ -25,70 +30,66 @@
 </template>
 
 <script>
-	import { mapActions, mapGetters } from 'vuex'
-	import toast from '@/mixins/toast.mixin'
-	import RecipeEditView from '@/components/RecipeEditView.vue'
-	import RecipeTextView from '@/components/RecipeTextView.vue'
-	import {
-		GET_RECIPE,
-		GET_RECIPE_INGREDIENTS
-	} from '@/store/actions.type'
+import { mapActions, mapGetters } from "vuex"
+import toast from "@/mixins/toast.mixin"
+import RecipeEditView from "@/components/RecipeEditView.vue"
+import RecipeTextView from "@/components/RecipeTextView.vue"
+import { GET_RECIPE, GET_RECIPE_INGREDIENTS } from "@/store/actions.type"
 
-	export default {
-		name: 'Recipe',
-		components: {
-			RecipeEditView, RecipeTextView
+export default {
+	name: "Recipe",
+	components: {
+		RecipeEditView,
+		RecipeTextView,
+	},
+	mixins: [toast],
+	data() {
+		return {
+			tabIndex: 0,
+			tabs: ["#text-view", "#edit-view"],
+		}
+	},
+	methods: {
+		...mapActions({
+			getRecipe: `recipe/${GET_RECIPE}`,
+			getRecipeIngredients: `recipe/${GET_RECIPE_INGREDIENTS}`,
+		}),
+		tabCheck() {
+			this.$nextTick(() => {
+				this.tabIndex = this.tabs.findIndex(
+					(tab) => tab === this.$route.hash
+				)
+			})
 		},
-		mixins: [toast],
-		data() {
-			return {
-				tabIndex: 0,
-				tabs: ['#text-view', '#edit-view']
-			};
-		},
-		methods: {
-			...mapActions({
-				getRecipe: `recipe/${GET_RECIPE}`,
-				getRecipeIngredients: `recipe/${GET_RECIPE_INGREDIENTS}`
-			}),
-			tabCheck() {
-				this.$nextTick(() => {
-					this.tabIndex = this.tabs.findIndex(tab => tab === this.$route.hash)
-				})
+	},
+	computed: {
+		...mapGetters({
+			currentRecipe: "recipe/currentRecipe",
+			ingredientsToShow: "recipe/currentRecipeIngredients",
+		}),
+		ingredientsText() {
+			if (!Array.isArray(this.ingredientsToShow)) {
+				return
 			}
-		},
-		computed: {
-			...mapGetters({
-				currentRecipe: 'recipe/currentRecipe',
-				ingredientsToShow: 'recipe/currentRecipeIngredients',
-			}),
-			ingredientsText() {
-				if (! Array.isArray(this.ingredientsToShow)) {
-					return;
+			let ingredients = this.ingredientsToShow.map((x) => {
+				let text = `${x.name} - ${x.quantity}`
+				if (x.notes) {
+					text += " (" + x.notes + ")"
 				}
-				let ingredients = this.ingredientsToShow.map(x => {
-					let text = `${x.name} - ${x.quantity}`;
-					if (x.notes) {
-						text += ' (' + x.notes + ')';
-					}
-					return text;
-				})
-				return ingredients.join(' \n')
-			}
+				return text
+			})
+			return ingredients.join(" \n")
 		},
-		created() {
-		},
-		mounted() {
-			let id = this.$route.params.id
-			this.tabCheck()
-			this.getRecipe(id)
-				.then(() => this.getRecipeIngredients(id))
-				.catch(error => this.$toastError(error, `Getting RecipeId ${id}`))
-		},
-	}
-
+	},
+	created() {},
+	mounted() {
+		let id = this.$route.params.id
+		this.tabCheck()
+		this.getRecipe(id)
+			.then(() => this.getRecipeIngredients(id))
+			.catch((error) => this.$toastError(error, `Getting RecipeId ${id}`))
+	},
+}
 </script>
 
-<style scoped>
-</style>
-
+<style scoped></style>
